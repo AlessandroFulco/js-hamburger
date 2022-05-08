a4a42178aee1e49a38f6fbb72fa4775e67cdb906
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-05-08 15:24:27 +0200

    esercizio finalizzato

---
+++ main.js
@@ -0,0 +1,26 @@
+// mostrare e nascondere il menu al click
+
+//creare una funzione che al click aggiunge 
+// la classe .active al div.hamburger-menu
+
+const button = document.querySelector('.fa-bars');
+console.log(button);
+
+const menu  = document.querySelector('.hamburger-menu');
+console.log(menu);
+
+// aprire il menu ad hamburger
+button.addEventListener('click',
+    function() {
+        menu.classList.add('active');
+    }
+);
+
+// chiudere il menu ad hamburger al click o in base al viewport superiore a 1000px
+menu.addEventListener('click',
+    function() {
+        menu.classList.remove('active');
+    }
+);
+
+
